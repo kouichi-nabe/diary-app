a454ba394ef5a774faeb5350bb9141bf20cfdd04
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-12-14 18:06:24 +0900

    Add user views& functions
    ユーザー関連のviewや機能の追加、修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in' # サインアウト後のリダイレクト先URL
   end
 end
